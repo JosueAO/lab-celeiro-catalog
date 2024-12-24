1bea10e34ee2c32ea034b487ff4888007215f145
Josue A. Oliveira <josueao.mobile@gmail.com>
2024-12-24 03:57:25 -0300

    Backed project created

---
+++ backend/src/main/java/com/josueao/celeirocatalog/CeleirocatalogApplication.java
@@ -0,0 +1,13 @@
+package com.josueao.celeirocatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeleirocatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeleirocatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/josueao/celeirocatalog/CeleirocatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josueao.celeirocatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeleirocatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
